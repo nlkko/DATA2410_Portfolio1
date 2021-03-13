209da929bbc8111f3e4dab032fac01a8d81c179c
Nikola Dordevic <Dordenik1@gmail.com>
2021-03-13 20:02:27 +0100

    Main chat system
    Problems with getting clientConnection method to run in a thread

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+import threading
+from datetime import datetime
 
 address = sys.argv[1]
 port = int(sys.argv[2])
@@ -7,3 +9,13 @@ name = sys.argv[3]
 
 c = socket.socket()
 c.connect((address, port))
+
+def sendMessage():
+    msg = input("Send: ")
+    c.send(msg.encode())
+
+#OBS Må lage en egen thread for å sende og motta melding
+#while True:
+    #Client either recieves or sends a message to the server
+msg = input("SKriv melding:  ")
+c.send(msg.encode())
--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+import threading
+from datetime import datetime
 from commands import *
 
 address = sys.argv[1]
@@ -9,15 +11,44 @@ s = socket.socket()
 s.bind((address, port))
 s.listen()
 
-# List of clients that establish a connection
-clients = []
+# Client class that keeps track of attributes of each connected client
+class Client:
+    def __init__(this, connection, source, name = None):
+        this.connection = connection
+        this.source = source
+        #this.thread = thread
+        this.name = name if name is not None else "Client"
 
 # A method for client connections to be sent to a separate thread.
-def clientConnection(connection, source):
+def clientConnection(client):
     while True:
+        print("clientCOnnection")
+        #Client sends a message
+        msg = client.connection.recv(1024).decode()
+        time = datetime.now().strftime("%H:%M")
+        broadcastMsg = time +"   "+ client.name +": "+ msg
+        print(broadcastMsg)
 
+        # broadcast(broadcastMsg)
 
+# Method that sends a message to all clients
+#def broadcast():
+    # Send message to all clients
 
+#def command():
+    # Client runs a command
+
+# List of clients that have established a connection
+clients = []
+
+# Establishes a connection and creates a thread for each client
 while True:
-    connection, address = s.accept()
-    clients.append(connection)
+    connection, source = s.accept()
+    client = Client(connection, source)
+    #client = Client(connection, source, threading.Thread(target=clientConnection, args=(client,))
+    print(client.source[0] +" connected!")
+
+    clients.append(client)
+    print(clients)
+    t = threading.Thread(target=clientConnection, args=(client,))
+    t.start()
